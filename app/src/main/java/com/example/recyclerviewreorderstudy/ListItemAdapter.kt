6e202ea465897f546b51d5cf518de1bd6fa888cc
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2023-01-30 09:00:19 +0900

    テキスト変更通知の修正
    - ソフトウェアキーボードを閉じたとき
    - 別の Viewにフォーカスが移動したとき

--- app/src/main/java/com/example/recyclerviewreorderstudy/ListItemAdapter.kt
+++ app/src/main/java/com/example/recyclerviewreorderstudy/ListItemAdapter.kt
@@ -1,13 +1,12 @@
 package com.example.recyclerviewreorderstudy
 
 import android.content.Context
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
-import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
 
 /**
@@ -26,9 +25,9 @@ class ListItemAdapter :
 
     private val dataSet = mutableListOf<ListItem>()
 
-    var callback : Callback? = null
+    var callback: Callback? = null
 
-    fun setData(newItems : List<ListItem>) {
+    fun setData(newItems: List<ListItem>) {
         dataSet.clear()
         dataSet.addAll(newItems)
         notifyDataSetChanged()
@@ -36,23 +35,6 @@ class ListItemAdapter :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-
-        val etTitle = view.findViewById<EditText>(R.id.etTitle)
-
-        etTitle.setOnKeyListener { v, keyCode, event ->
-            if ((event.action == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                val inputMethodManager =
-                    parent.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                inputMethodManager.hideSoftInputFromWindow(
-                    v.windowToken,
-                    InputMethodManager.RESULT_UNCHANGED_SHOWN
-                )
-
-                return@setOnKeyListener true
-            }
-            return@setOnKeyListener false
-        }
-
         return ListItemViewHolder(view)
     }
 
@@ -61,24 +43,40 @@ class ListItemAdapter :
 
         holder.etTitle.setText(listItem.title)
 
-        holder.onTextChanged = {}
-        holder.etTitle.setText(dataSet[position].title)
-        holder.onTextChanged = {
-            // dataSet[position].title = it
-            callback?.onTextChanged(position, it)
+        holder.etTitle.setOnEditorActionListener { v, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // ソフトウェアキーボードを閉じる
+                val inputMethodManager =
+                    holder.etTitle.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+                inputMethodManager.hideSoftInputFromWindow(
+                    v.windowToken,
+                    InputMethodManager.RESULT_UNCHANGED_SHOWN
+                )
+
+                // テキストの変更を通知
+                callback?.onTextChanged(
+                    position,
+                    holder.etTitle.text.toString()
+                )
+
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+
+        // フォーカスを外れたらテキストの変更を通知
+        holder.etTitle.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                callback?.onTextChanged(position, holder.etTitle.text.toString())
+            }
         }
     }
 
     override fun getItemCount(): Int = dataSet.size
 
-    class ListItemViewHolder(view: View, var onTextChanged: ((String) -> Unit) = {}) :
+    class ListItemViewHolder(view: View) :
         RecyclerView.ViewHolder(view) {
         val etTitle: EditText = view.findViewById(R.id.etTitle)
-
-        init {
-            etTitle.doOnTextChanged { text, _, _, _ ->
-                onTextChanged(text.toString())
-            }
-        }
     }
 }
